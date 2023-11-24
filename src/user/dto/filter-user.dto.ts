92f67820cb39cb25c6bc94b65f2f876f952af82a
cfardev <cfardev@gmail.com>
2023-11-23 22:39:06 -0600

    Feature: users

--- src/user/dto/filter-user.dto.ts
+++ src/user/dto/filter-user.dto.ts
@@ -1,12 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@prisma/client';
 import { Transform } from 'class-transformer';
 import { IsOptional, IsString, IsEnum } from 'class-validator';
 
 export class FilterUserDto {
+  @ApiProperty({ required: false, description: 'Search by name or email' })
   @IsOptional()
   @IsString()
   search?: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'Filter by role',
+    enum: UserRole,
+    isArray: true,
+  })
   @IsOptional()
   @IsEnum(UserRole, { each: true })
   @Transform(({ value }) => value.split(','), { toClassOnly: true })
